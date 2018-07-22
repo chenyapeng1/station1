5175a1a58ad964b1754540f117c79a3fad614f49
chenyapeng1 <18335442530@163.com>
2018-07-22 22:29:49 +0800

    cyp5

---
+++ js/index.js
@@ -0,0 +1,17 @@
+window.onload=function () {
+    let class_shang=document.getElementsByClassName("class_shang")[0];
+    let selection=class_shang.getElementsByClassName("selection");
+    let menu_drop=class_shang.getElementsByClassName("menu_drop");
+    for (let i=0;i<selection.length;i++){
+        selection[i].onmouseenter=function(){
+            for (let j=0;j<selection.length;j++){
+                menu_drop[j].style.height=0;
+            }
+            menu_drop[i].style.height="212px";
+            menu_drop[i].style.display="block";
+        }
+        selection[i].onmouseleave=function(){
+            menu_drop[i].style.height=0;
+        }
+    }
+}
